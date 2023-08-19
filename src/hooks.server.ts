af55b1b19eb54353d9dd3840baf4f0f4fbb26cca
fajar sidik priatna <jardik.oryza@gmail.com>
2023-08-20 01:52:00 +0700

    fix logger

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,9 @@ import { logger } from '$lib/logger';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const resp = await resolve(event);
-	if (resp.status === 400) {
-		const data = await resp.json();
-        logger.error(data.message);
+	const resp: Response = await resolve(event);
+	if (resp.status >= 400) {
+        logger.info(await resp.clone().json());
 	}
 	return resp
 };
